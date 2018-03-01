592883ef7dd138cb8fc01670a7362936e0b4f65f
Grace Chen <gpc001@ucsd.edu>
2018-02-28 23:42:16 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,25 +2,54 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  initializePage();
+	initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	console.log("Page ready");
+ function initializePage() {
+ 	console.log("Page ready");
  	initCamera();
  	initGestures();
  	initRSVPForm();
-}
+ }
+
+// Swipe Event Listener
+// $(function() {
+// 	$('div.box').bind('taphold', tapholdHandler);
+// 	function tapholdHander( event ){
+// 		$event.target.addClass('taphold');
+// 	}
+// });
 
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function() {
+		$('.judge-img').bind('taphold', tapholdHandler);
+		function tapholdHandler( event ){
+			var targetIDPrefix = event.target.id;
+			console.log('got prefix: ' + targetIDPrefix);
+			$('#' + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+  	e.preventDefault();
+  	console.log("submitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	// send POST request
+  	$.post('addRSVP', {rsvpEmail: rsvpEmail }, postCallback);
+  });
+function postCallback(res) {
+	alert('RSVP form successfully submitted!');
+	$('#rsvpEmail').val('');
 }
+
+}
+
